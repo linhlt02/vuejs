67f8a24774d90c4f33c928b3f43e952178b51ba1
linhlt02 <linhlt02.it@gmail.comm>
2022-09-13 11:56:33 +0700

    setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Quản lí </h1>
+    <ProductView />
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Price($)</th>
+          <th scope="col">Desc</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list" :key="item.id">
+          <th>{{item.id}}</th>
+          <td>{{item.name}}</td>
+          <td>{{item.price}}</td>
+          <td>{{item.desc}}</td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  import ProductView from "./Product"
+  export default {
+    components: {
+      ProductView
+    },
+    data() {
+      return {
+        list: [
+          { id: 1, name: "Product A", price: "10", desc: "Blue" },
+          { id: 2, name: "Product B", price: "5", desc: "Pink" },
+          { id: 3, name: "Product C", price: "9", desc: "Green" }
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-prevent-closing>Add Product </b-button>
+
+    <!-- <div class="mt-3">
+      Submitted Names:
+      <div v-if="submittedNames.length === 0">--</div>
+      <ul v-else class="mb-0 pl-3">
+        <li v-for="(name, index) in submittedNames" :key="index">{{ name }}</li>
+      </ul>
+    </div> -->
+
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Submit Your Name"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          label="Name"
+        >
+          <b-form-input
+          ></b-form-input>
+        </b-form-group>
+      </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProductView',
+    data() {
+      return {
+        name: '',
+        nameState: null,
+        submittedNames: []
+      }
+    },
+    methods: {
+      checkFormValidity() {
+        const valid = this.$refs.form.checkValidity()
+        this.nameState = valid
+        return valid
+      },
+      resetModal() {
+        this.name = ''
+        this.nameState = null
+      },
+      handleOk(bvModalEvent) {
+        // Prevent modal from closing
+        bvModalEvent.preventDefault()
+        // Trigger submit handler
+        this.handleSubmit()
+      },
+      handleSubmit() {
+        // Exit when the form isn't valid
+        if (!this.checkFormValidity()) {
+          return
+        }
+        // Push the name to submitted names
+        this.submittedNames.push(this.name)
+        // Hide the modal manually
+        this.$nextTick(() => {
+          this.$bvModal.hide('modal-prevent-closing')
+        })
+      }
+    }
+  }
+</script>
